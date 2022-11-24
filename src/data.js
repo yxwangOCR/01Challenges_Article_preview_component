27697b0eb4c066173c7a0dbc842bcf1d20ecf63b
Yixuan WANG <wang.yixuan@live.fr>
2022-11-24 13:17:11 -0500

    Add content and images

--- src/data.js
+++ src/data.js
@@ -1,7 +1,11 @@
-import Img1 from "./images/drawers.jpg";
+import Img1 from "./images/Home1.jpg";
+import Img2 from "./images/Home2.jpg";
+import Img3 from "./images/Home3.jpg";
+import Img4 from "./images/Home4.jpg";
 import Profile1 from "./images/avatar-michelle.jpg";
-import Img2 from "./images/architecture.jpg";
 import Profile2 from "./images/avatar-campbell.jpg";
+import Profile3 from "./images/avatar-joseph.jpg";
+import Profile4 from "./images/avatar-julie.jpg";
 const articale = [
   {
     title:
@@ -17,11 +21,31 @@ const articale = [
     title:
       "Do you ever find yourself in a room and it just feels like something’s missing? Well, I have the easy fix for you! ",
     name: "Campbell Simpson",
-    date: "November 20, 2022",
+    date: "November 20, 2020",
     imgUrl: Img2,
     profile: Profile2,
     description:
       "Did you ever feel your living room, bedroom or any other room in your home was missing something, even though it was complete? We’ll go over 5 tips that will help transform even the most boring room into a cozy haven.",
+  },
+  {
+    title:
+      "Des choses simples peuvent avoir un impact important sur l'aspect général et la sensation de n'importe quelle pièce. ",
+    name: "Joseph Gonzalez",
+    date: "November 10, 2020",
+    imgUrl: Img3,
+    profile: Profile3,
+    description:
+      "Avec quelques accessoires et des pièces bien pensées, vous pouvez faire en sorte que votre espace soit complet, accueillant et chaleureux.",
+  },
+  {
+    title:
+      "Vous pouvez transformer instantanément l'aspect et la sensation de n'importe quelle pièce de votre maison en ajoutant quelques touches simples. ",
+    name: "Julie Levrone",
+    date: "July 26, 2021",
+    imgUrl: Img4,
+    profile: Profile4,
+    description:
+      "Qu'il s'agisse de décorer avec des plantes ou d'ajouter des rangements supplémentaires, ces idées testées et éprouvées feront toute la différence.",
   },
 ];
 
